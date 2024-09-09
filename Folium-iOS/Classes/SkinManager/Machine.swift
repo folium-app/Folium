7e7b8bda9beb67a5317d86252bee1f13afbc78ba
Jarrod Norwell <official.antique@gmail.com>
2024-09-09 14:12:12 +0800

    Update Machine.swift
    Added all iPad and iPhone models that support *OS 16 and above and improved naming of cases

--- Folium-iOS/Classes/SkinManager/Machine.swift
+++ Folium-iOS/Classes/SkinManager/Machine.swift
@@ -9,165 +9,246 @@
 import Foundation
 
 enum Machine : String, Codable, CaseIterable, Hashable {
-    // iPad Pro 12.9-inch series
-    case iPadPro12_9_1stGen = "iPad6,7"  // iPad Pro 12.9-inch (1st generation)
-    case iPadPro12_9_2ndGen = "iPad6,8"  // iPad Pro 12.9-inch (2nd generation)
-    case iPadPro12_9_3rdGen = "iPad8,5"  // iPad Pro 12.9-inch (3rd generation)
-    case iPadPro12_9_4thGen = "iPad8,11" // iPad Pro 12.9-inch (4th generation)
-    case iPadPro12_9_5thGen = "iPad13,8" // iPad Pro 12.9-inch (5th generation)
-    case iPadPro12_9_6thGen = "iPad14,5" // iPad Pro 12.9-inch (6th generation)
-    case iPadPro12_9_7thGen = "iPad16,5" // iPad Pro 12.9-inch (7th generation)
+    case iPad10Gen_1 = "iPad13,19"
+    case iPad10Gen_2 = "iPad13,18"
 
-    // iPad Pro 11-inch series
-    case iPadPro11_1stGen = "iPad8,1"    // iPad Pro 11-inch (1st generation)
-    case iPadPro11_2ndGen = "iPad8,3"    // iPad Pro 11-inch (2nd generation)
-    case iPadPro11_3rdGen = "iPad8,9"    // iPad Pro 11-inch (3rd generation)
-    case iPadPro11_4thGen = "iPad8,10"   // iPad Pro 11-inch (4th generation)
-    case iPadPro11_5thGen = "iPad13,4"   // iPad Pro 11-inch (5th generation)
-    case iPadPro11_6thGen = "iPad13,5"   // iPad Pro 11-inch (5th generation)
-    case iPadPro11_7thGen = "iPad16,3"   // iPad Pro 11-inch (5th generation)
+    case iPad9Gen_1 = "iPad12,1"
+    case iPad9Gen_2 = "iPad12,2"
 
-    // iPad (10th generation)
-    case iPad10thGen = "iPad13,18"  // iPad (10th generation)
+    case iPad8Gen_1 = "iPad11,6"
+    case iPad8Gen_2 = "iPad11,7"
 
-    // iPad (9th generation)
-    case iPad9thGen = "iPad12,1"   // iPad (9th generation)
+    case iPad7Gen_1 = "iPad7,11"
+    case iPad7Gen_2 = "iPad7,12"
 
-    // iPad (8th generation)
-    case iPad8thGen = "iPad11,6"   // iPad (8th generation)
+    case iPad6Gen_1 = "iPad7,5"
+    case iPad6Gen_2 = "iPad7,6"
 
-    // iPad (7th generation)
-    case iPad7thGen = "iPad7,11"   // iPad (7th generation)
+    case iPad5Gen_1 = "iPad6,11"
+    case iPad5Gen_2 = "iPad6,12"
 
-    // iPad (6th generation)
-    case iPad6thGen = "iPad7,5"    // iPad (6th generation)
+    case iPadAir6Gen_1 = "iPad14,10" // 13-in (M2)
+    case iPadAir6Gen_2 = "iPad14,11" // 13-in (M2)
 
-    // iPad (5th generation)
-    case iPad5thGen = "iPad6,11"   // iPad (5th generation)
+    case iPadAir6Gen_3 = "iPad14,18" // 11-in (M2)
+    case iPadAir6Gen_4 = "iPad14,19" // 11-in (M2)
 
-    // iPad mini series
-    case iPadMini5thGen = "iPad11,1"  // iPad mini (5th generation)
-    case iPadMini6thGen = "iPad14,1"  // iPad mini (6th generation)
-
-    // iPad Air series
-    case iPadAir3rdGen = "iPad11,3"   // iPad Air (3rd generation)
-    case iPadAir4thGen = "iPad13,1"   // iPad Air (4th generation)
-    case iPadAir5thGen = "iPad13,16"  // iPad Air (5th generation)
-    case iPadAir6thGen = "iPad14,8"   // iPad Air (6th generation)
-    case iPadAir7thGen = "iPad14,10"  // iPad Air (7th generation)
+    case iPadAir5Gen_1 = "iPad13,16"
+    case iPadAir5Gen_2 = "iPad13,17"
     
-    
-    // iPhone 14 series
-    case iPhone14 = "iPhone14,7"  // iPhone 14
-    case iPhone14Plus = "iPhone14,8"  // iPhone 14 Plus
-    case iPhone14Pro = "iPhone15,2"  // iPhone 14 Pro
-    case iPhone14ProMax = "iPhone15,3"  // iPhone 14 Pro Max
+    case iPadAir4Gen_1 = "iPad13,1"
+    case iPadAir4Gen_2 = "iPad13,2"
 
-    // iPhone 13 series
-    case iPhone13 = "iPhone14,5"  // iPhone 13
-    case iPhone13Mini = "iPhone14,4"  // iPhone 13 Mini
-    case iPhone13Pro = "iPhone14,2"  // iPhone 13 Pro
-    case iPhone13ProMax = "iPhone14,3"  // iPhone 13 Pro Max
+    case iPadMini6Gen_1 = "iPad14,1"
+    case iPadMini6Gen_2 = "iPad14,2"
 
-    // iPhone 12 series
-    case iPhone12 = "iPhone13,2"  // iPhone 12
-    case iPhone12Mini = "iPhone13,1"  // iPhone 12 Mini
-    case iPhone12Pro = "iPhone13,3"  // iPhone 12 Pro
-    case iPhone12ProMax = "iPhone13,4"  // iPhone 12 Pro Max
+    case iPadMini5Gen_1 = "iPad11,1"
+    case iPadMini5Gen_2 = "iPad11,2"
 
-    // iPhone 11 series
-    case iPhone11 = "iPhone12,1"  // iPhone 11
-    case iPhone11Pro = "iPhone12,3"  // iPhone 11 Pro
-    case iPhone11ProMax = "iPhone12,5"  // iPhone 11 Pro Max
+    case iPadPro_1 = "iPad6,7"
+    case iPadPro_2 = "iPad6,8"
 
-    // iPhone XS series
-    case iPhoneXS = "iPhone11,2"  // iPhone XS
-    case iPhoneXSMax = "iPhone11,4"  // iPhone XS Max
-    case iPhoneXSMaxGlobal = "iPhone11,6"  // iPhone XS Max Global
+    case iPadPro_3 = "iPad6,3"
+    case iPadPro_4 = "iPad6,4"
 
-    // iPhone XR
-    case iPhoneXR = "iPhone11,8"  // iPhone XR
+    case iPadPro2Gen_1 = "iPad7,1"
+    case iPadPro2Gen_2 = "iPad7,2"
 
-    // iPhone X
-    case iPhoneX = "iPhone10,3"  // iPhone X Global
-    case iPhoneXGSM = "iPhone10,6"  // iPhone X GSM
+    case iPadPro2Gen_3 = "iPad7,3"
+    case iPadPro2Gen_4 = "iPad7,4"
 
-    // iPhone 8 series
-    case iPhone8 = "iPhone10,1"  // iPhone 8
-    case iPhone8Plus = "iPhone10,2"  // iPhone 8 Plus
+    case iPadPro3Gen_1 = "iPad8,1"
+    case iPadPro3Gen_2 = "iPad8,2"
 
-    // iPhone SE series
-    case iPhoneSE2 = "iPhone12,8"  // iPhone SE (2nd generation)
-    case iPhoneSE3 = "iPhone14,6"  // iPhone SE (3rd generation)
+    case iPadPro3Gen_3 = "iPad8,3"
+    case iPadPro3Gen_4 = "iPad8,4"
 
-    // iPhone 15 series
-    case iPhone15 = "iPhone15,4"  // iPhone 15
-    case iPhone15Plus = "iPhone15,5"  // iPhone 15 Plus
-    case iPhone15Pro = "iPhone16,1"  // iPhone 15 Pro
-    case iPhone15ProMax = "iPhone16,2"  // iPhone 15 Pro Max
-    
+    case iPadPro3Gen_5 = "iPad8,5"
+    case iPadPro3Gen_6 = "iPad8,6"
+
+    case iPadPro3Gen_7 = "iPad8,7"
+    case iPadPro3Gen_8 = "iPad8,8"
+
+    case iPadPro4Gen_1 = "iPad8,9"
+    case iPadPro4Gen_2 = "iPad8,10"
+
+    case iPadPro5Gen_1 = "iPad8,11"
+    case iPadPro5Gen_2 = "iPad8,12"
+
+    case iPadPro5Gen_3 = "iPad13,4"
+    case iPadPro5Gen_4 = "iPad13,5"
+
+    case iPadPro5Gen_5 = "iPad13,6"
+    case iPadPro5Gen_6 = "iPad13,7"
+
+    case iPadPro5Gen_7 = "iPad13,8"
+    case iPadPro5Gen_8 = "iPad13,9"
+
+    case iPadPro5Gen_9 = "iPad13,10"
+    case iPadPro5Gen_10 = "iPad13,11"
+
+    case iPadPro6Gen_1 = "iPad" // TODO: iPad Pro (11-inch) (4th generation)
+    case iPadPro6Gen_2 = "iPad" // TODO: iPad Pro (11-inch) (4th generation)
+
+    case iPadPro6Gen_3 = "iPad" // TODO: iPad Pro (11-inch) (4th generation)
+    case iPadPro6Gen_4 = "iPad" // TODO: iPad Pro (11-inch) (4th generation)
+
+    case iPadPro6Gen_1 = "iPad" // TODO: iPad Pro (12.9-inch) (6th generation)
+    case iPadPro6Gen_2 = "iPad" // TODO: iPad Pro (12.9-inch) (6th generation)
+
+    case iPadPro6Gen_3 = "iPad" // TODO: iPad Pro (12.9-inch) (6th generation)
+    case iPadPro6Gen_4 = "iPad" // TODO: iPad Pro (12.9-inch) (6th generation)
+
+    case iPadPro7Gen_1 = "iPad16,3" // 11-in (M4)
+    case iPadPro7Gen_2 = "iPad16,4" // 11-in (M4)
+
+    case iPadPro7Gen_1 = "iPad16,5" // 13-in (M4)
+    case iPadPro7Gen_2 = "iPad16,6" // 13-in (M4)
+
+
+    case iPhone8_1 = "iPhone10,1"
+    case iPhone8_2 = "iPhone10,4"
+
+    case iPhone8_3 = "iPhone10,2"
+    case iPhone8_4 = "iPhone10,5"
+
+    case iPhoneX_1 = "iPhone10,3"
+    case iPhoneX_2 = "iPhone10,6"
+
+    case iPhoneXR_1 = "iPhone11,8"
+
+    case iPhoneXS_1 = "iPhone11,2"
+    case iPhoneXS_2 = "iPhone11,4" // Max
+    case iPhoneXS_3 = "iPhone11,6" // Max
+
+    case iPhone11_1 = "iPhone12,1"
+    case iPhone11_2 = "iPhone12,3" // Pro
+    case iPhone11_3 = "iPhone12,5" // Pro Max
+
+    case iPhoneSE2Gen_1 = "iPhone12,8"
+    case iPhoneSE3Gen_1 = "iPhone14,6"
+
+    case iPhone12_1 = "iPhone13,1" // mini
+    case iPhone12_2 = "iPhone13,2"
+    case iPhone12_3 = "iPhone13,3" // Pro
+    case iPhone12_4 = "iPhone13,4" // Pro Max
+
+    case iPhone13_1 = "iPhone14,4" // mini
+    case iPhone13_2 = "iPhone14,5"
+    case iPhone13_3 = "iPhone14,2" // Pro
+    case iPhone13_4 = "iPhone14,3" // Pro Max
+
+    case iPhone14_1 = "iPhone14,7"
+    case iPhone14_2 = "iPhone14,8" // Plus
+    case iPhone14_3 = "iPhone15,2" // Pro
+    case iPhone14_4 = "iPhone15,3" // Pro Max
+
+    case iPhone15_1 = "iPhone15,4"
+    case iPhone15_2 = "iPhone15,5" // Plus
+    case iPhone15_3 = "iPhone16,1" // Pro
+    case iPhone15_4 = "iPhone16,2" // Pro Max
+
     static let iPad: [Machine] = [
-        .iPadPro12_9_1stGen,
-        .iPadPro12_9_2ndGen,
-        .iPadPro12_9_3rdGen,
-        .iPadPro12_9_4thGen,
-        .iPadPro12_9_5thGen,
-        .iPadPro12_9_6thGen,
-        .iPadPro12_9_7thGen,
-        .iPadPro11_1stGen,
-        .iPadPro11_2ndGen,
-        .iPadPro11_3rdGen,
-        .iPadPro11_4thGen,
-        .iPadPro11_5thGen,
-        .iPadPro11_6thGen,
-        .iPadPro11_7thGen,
-        .iPad10thGen,
-        .iPad9thGen,
-        .iPad8thGen,
-        .iPad7thGen,
-        .iPad6thGen,
-        .iPad5thGen,
-        .iPadMini5thGen,
-        .iPadMini6thGen,
-        .iPadAir3rdGen,
-        .iPadAir4thGen,
-        .iPadAir5thGen,
-        .iPadAir6thGen,
-        .iPadAir7thGen
+        iPad10Gen_1,
+        iPad10Gen_2,
+        iPad9Gen_1,
+        iPad9Gen_2,
+        iPad8Gen_1,
+        iPad8Gen_2,
+        iPad7Gen_1,
+        iPad7Gen_2,
+        iPad6Gen_1,
+        iPad6Gen_2,
+        iPad5Gen_1,
+        iPad5Gen_2,
+        iPadAir6Gen_1,
+        iPadAir6Gen_2,
+        iPadAir6Gen_3,
+        iPadAir6Gen_4,
+        iPadAir5Gen_1,
+        iPadAir5Gen_2,
+        iPadAir4Gen_1,
+        iPadAir4Gen_2,
+        iPadMini6Gen_1,
+        iPadMini6Gen_2,
+        iPadMini5Gen_1,
+        iPadMini5Gen_2,
+        iPadPro_1,
+        iPadPro_2,
+        iPadPro_3,
+        iPadPro_4,
+        iPadPro2Gen_1,
+        iPadPro2Gen_2,
+        iPadPro2Gen_3,
+        iPadPro2Gen_4,
+        iPadPro3Gen_1,
+        iPadPro3Gen_2,
+        iPadPro3Gen_3,
+        iPadPro3Gen_4,
+        iPadPro3Gen_5,
+        iPadPro3Gen_6,
+        iPadPro3Gen_7,
+        iPadPro3Gen_8,
+        iPadPro4Gen_1,
+        iPadPro4Gen_2,
+        iPadPro5Gen_1,
+        iPadPro5Gen_2,
+        iPadPro5Gen_3,
+        iPadPro5Gen_4,
+        iPadPro5Gen_5,
+        iPadPro5Gen_6,
+        iPadPro5Gen_7,
+        iPadPro5Gen_8,
+        iPadPro5Gen_9,
+        iPadPro5Gen_10,
+        iPadPro6Gen_1,
+        iPadPro6Gen_2,
+        iPadPro6Gen_3,
+        iPadPro6Gen_4,
+        iPadPro6Gen_1,
+        iPadPro6Gen_2,
+        iPadPro6Gen_3,
+        iPadPro6Gen_4,
+        iPadPro7Gen_1,
+        iPadPro7Gen_2,
+        iPadPro7Gen_1,
+        iPadPro7Gen_2
     ]
-    
-    static let iPhone_mini: [Machine] = [
-        .iPhoneSE2,
-        .iPhoneSE3,
-        .iPhone12Mini,
-        .iPhone13Mini
-    ]
-    
+
     static let iPhone: [Machine] = [
-        .iPhone8,
-        .iPhone8Plus,
-        .iPhoneX,
-        .iPhoneXGSM,
-        .iPhoneXR,
-        .iPhoneXS,
-        .iPhoneXSMax,
-        .iPhoneXSMaxGlobal,
-        .iPhone11,
-        .iPhone11Pro,
-        .iPhone11ProMax,
-        .iPhone12,
-        .iPhone12Pro,
-        .iPhone12ProMax,
-        .iPhone13,
-        .iPhone13Pro,
-        .iPhone13ProMax,
-        .iPhone14,
-        .iPhone14Plus,
-        .iPhone14Pro,
-        .iPhone14ProMax,
-        .iPhone15,
-        .iPhone15Plus,
-        .iPhone15Pro,
-        .iPhone15ProMax
+        iPhone8_1,
+        iPhone8_2,
+        iPhone8_3,
+        iPhone8_4,
+        iPhoneX_1,
+        iPhoneX_2,
+        iPhoneXR_1,
+        iPhoneXS_1,
+        iPhoneXS_2,
+        iPhoneXS_3,
+        iPhone11_1,
+        iPhone11_2,
+        iPhone11_3,
+        iPhone12_2,
+        iPhone12_3,
+        iPhone12_4,
+        iPhone13_2,
+        iPhone13_3,
+        iPhone13_4,
+        iPhone14_1,
+        iPhone14_2,
+        iPhone14_3,
+        iPhone14_4,
+        iPhone15_1,
+        iPhone15_2,
+        iPhone15_3,
+        iPhone15_4
+    ]
+
+    static let iPhone_mini: [Machine] = [
+        iPhoneSE2Gen_1,
+        iPhoneSE3Gen_1,
+        iPhone12_1,
+        iPhone13_1
     ]
 }
